949aa233c0e1a4ee5b80192fd8b188ea51c2fb6b
Omar Alomar <omaro2000@gmail.com>
2022-01-19 20:16:56 -0500

    navbar rough draft

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import { Layout, Typography, Menu } from 'antd';
+
+const { Header, Footer, Content } = Layout;
+const { Title } = Typography;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="app">
+      <Layout className="layout">
+        <Header className="header">
+        <div className="logo-wrapper"><img src="logo.png" className="logo"/></div>
+          
+          <Menu mode="horizontal" theme="dark" className="menu">
+            <Menu.Item className="menu-item" key="about">
+              About
+            </Menu.Item>
+            <Menu.Item className="menu-item" key="contact">
+              Contact
+            </Menu.Item>
+            <Menu.Item className="menu-item" key="community">
+              Mining Community
+            </Menu.Item>
+          </Menu>
+        </Header>
+
+        <Content>
+          <div className="site-layout-content">
+            Content
+          </div>
+        </Content>
+
+        <Footer style={{ textAlign: 'center' }}>Kripto Kyng ©2022 Created by Omar Alomar (rough draft)</Footer>
+      </Layout>
     </div>
+    </>
   );
 }
 
